88818590577c5568afa661d399bb7463e77eb97d
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-03-23 08:28:24 +0100

    add travis, modify test to test on rendu

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,8 @@ TEST_AGAINST=https://github.com/mfortin42/fillit.git
 REGEN=true
 MIN=1
 MAX=10
+HISDIR=/tmp/fillit-test
+MYDIR=/tmp/fillit
 
 BLACK="\033[0;30m"
 RED="\033[0;31m"
@@ -17,9 +19,7 @@ END="\033[0m"
 
 cd test
 
-if [ ! -d "fillit-test" ]; then
-	git clone $TEST_AGAINST fillit-test
-fi
+git clone $TEST_AGAINST $HISDIR
 
 for arg in "$@"
 do
@@ -38,7 +38,9 @@ done
 
 echo "Testing against version: https://github.com/mfortin42/fillit.git"
 echo
-make -s -C fillit-test
+make -s -C $HISDIR
+PROJECT_DIR=$MYDIR make -s -C ../ rendu > /dev/null
+make -s -C $MYDIR
 
 echo $BLUE Range: $MIN $MAX $END
 for i in $(seq $MIN $MAX)
@@ -47,14 +49,17 @@ do
 	if [ "$REGEN" = true ]; then
 		../util/gen.pl $i > $i.tetris
 	fi
-	MY=$(time ../fillit $i.tetris)
-	HIS=$(time fillit-test/fillit $i.tetris)
+	MY=$(time $MYDIR/fillit $i.tetris)
+	HIS=$(time $HISDIR/fillit $i.tetris)
 	echo My:"\t"$RED $MY $END
 	echo His:"\t"$CYAN $HIS $END
 	if [ ! "$MY" = "$HIS" ]; then
 		echo $RED WRONG FOR $i.tetris $END
+		exit 1
 	else
 		echo $GREEN OK $END
 	fi
 	echo
 done
+
+exit 0
